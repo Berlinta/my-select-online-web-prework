d2c8581974fe201d76af242efcb95bd37c569522
Nash Brancic <nashfft@gmail.com>
2019-02-06 03:03:28 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,11 @@
 def my_select(collection)
   i = 0
-  kim = []
+  nash = []
   while i < collection.length
   if yield(collection[i]) == true
-    kim.push(collection[i])
+    nash.push(collection[i])
   end
   i += 1
 end
-kim
+nash
 end
